7ee8e59a758652e42517a72fc021de2608597830
cdemoulins <cdemoulins@70afb4c4-ad63-4337-a718-f2380ba28599>
2009-11-20 16:44:28 +0000

    Change the name to Archdep and correct package description.
    git-svn-id: https://indefero.archivel.fr/svn/archdep/trunk@4 70afb4c4-ad63-4337-a718-f2380ba28599

--- src/Archdep/setup.py
+++ src/Archdep/setup.py
@@ -5,10 +5,10 @@ from distutils.core import setup
 
 setup(name='ArchDep',
       version='0.1',
-      description='Check useful dependences in PKGBUILD or from package name.',
+      description='Check redundant dependences in PKGBUILD or from package name.',
       author='Clément DEMOULINS',
       author_email='cdemoulins@gmail.com',
-      url='http://trac2.assembla.com/cdemoulins/browser/Python/src/ArchDep/',
+      url='https://indefero.archivel.fr/index.php/p/archdep',
       py_modules=['Package'],
       license='GNU GPL 3',
       scripts=['archdep.py'],
